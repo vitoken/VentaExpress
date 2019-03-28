07ea2edd5434f23d340ad90af66a7ebeddbe68b6
victor <>
2019-03-27 22:37:27 -0300

    Ejercicio en clases de la venta express

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Venta Express</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div id="contenedor">
+            <div id="titulo"></div>
+            <div id="menu"><?php include('menu.php'); ?></div>
+            <div id="contenido"></div>
+        </div>
+
+    </body>
+    
+        <script>
+        $("#prisolicitud").on("click", function (event) {
+            if (($("[id*=subsolicitud]").css("display") != "none")) {
+                $("[id*=subsolicitud]").css("display", "none");
+            } else {
+                $("[id*=subsolicitud]").show();
+                $("[id*=subsolicitud]").css("display", "block");
+            }
+
+
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
+    
+</html>
---
+++ menu.php
@@ -0,0 +1,11 @@
+<div class="list-group">
+<a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Venta Express</a>
+<a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Solicitudes</a>
+<a id="subsolicitud" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Ingreso.php" ><i class="fa fa-sun-o fa-2x"></i> Ingreso</a>
+<a id="subsolicitud-lista" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Informes.php" ><i class="fa fa-sun-o fa-2x"></i> Informes</a>
+<a id="subsolicitud-lista" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Consultas.php" ><i class="fa fa-sun-o fa-2x"></i> Consultas</a>
+<a class="list-group-item" href="#"><i class="fa fa-birthday-cake fa-2x"></i> Cumpleaños</a>
+<a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> Certificados</a>
+<a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> Resumen</a>
+</div>
+      
