032c1205c98b4b620822eba650700313ddd62d5b
vitoken <>
2019-04-01 22:06:55 -0300

    Se crea el requerimiento. Pero con algunas dificultades en el diseño que deben ser corregidas.

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div class="list-group">
+<a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Venta Express</a>
+<a id="prisolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Solicitudes</a>
+<a id="subsolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Ingreso</a>
+<a id="subsolicitude" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Productos.php"><i class="fa fa-product-hunt"></i>Productos</a>
+<a id="subsolicitude" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Ventas.php"><i class="fa fa-product-hunt"></i>Ventas</a>
+<a id="subsolicitude" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Bodegas.php"><i class="fa fa-product-hunt"></i>Bodegas</a>
+<a id="subsolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Informes</a>
+<a id="subsolicitude" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Ventas.php"><i class="fa fa-product-hunt"></i>Ventas</a>
+<a id="subsolicitud" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Acceda a las solicitudes"><i class="fa fa-file-o fa-2x"></i> Consultas</a>
+<a id="subsolicitude" class="list-group-item submenu" href="<?=URLBASE;?>contenido/Productos2.php"><i class="fa fa-product-hunt"></i>Productos</a>
+</div>
+      
